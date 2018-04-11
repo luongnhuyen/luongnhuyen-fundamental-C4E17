1aaad404e4a8b6a27657825ce5d03c5b00a72c6f
luongnhuyen <luongnhuyen@gmail.com>
2018-04-11 22:45:11 +0700

    initial commit

---
+++ Session01/1.py
@@ -0,0 +1,17 @@
+from turtle import *
+speed(0)
+for i in range(99):
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+
+    left(28)
+
+
+
+mainloop()
---
+++ Session01/inputexample.py
@@ -0,0 +1,3 @@
+yob=int(input("Ban sinh nam bao nhieu?"))
+age=2018-yob
+print("Age",age)

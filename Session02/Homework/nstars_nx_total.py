8d4497c27a10e9e5e2eb98d10877c7b0ed250fd9
luongnhuyen <luongnhuyen@gmail.com>
2018-04-13 00:05:38 +0700

    initial commit

--- Session02/Homework/nstars_nx_total.py
+++ Session02/Homework/nstars_nx_total.py
@@ -1,3 +1,9 @@
 n=int(input("Nhap so"))
-for i in range (1,n+1,2):
-    print("* x",end=' ')
+a=int(n%2)
+if a == 1:
+    for i in range(1,n,2):
+        print("x *",end=' ')
+    print("x")
+else:
+    for i in range(1,n,2):
+        print("x *",end=' ')

095405c8c82398cda4b58955a5296ce31a0c9e24
luongnhuyen <luongnhuyen@gmail.com>
2018-04-23 23:10:05 +0700

    initial commit

--- Session05/Homework/exe_1_3.py
+++ Session05/Homework/exe_1_3.py
@@ -8,10 +8,10 @@ p = {
 # bx = 2
 # by = 2
 
-b = {
-    "x": 2,
-    "y": 2
-}
+# b = {
+#     "x": 2,
+#     "y": 2
+# }
 
 boxes = [
     {
@@ -24,56 +24,57 @@ boxes = [
     }
 ]
 
-print(boxes[0]["x"])
+# print(boxes[0]["x"])
 
 for box in boxes:
-    print(box["y"])
+    # print(box["x"])
+    # print(box["y"])
 
-while True:
-    for y in range(4):
-        for x in range(4):
-            if x == p["x"] and y == p["y"]:
-                print("P ", end="")
-            elif x == b["x"] and y == b["y"]:
-                print("B ", end="")
-            elif x == 1 and y == 3:
-                print("G ", end="")
-            else:
-                print("- ", end="")
-        print()
+    while True:
+        for y in range(4):
+            for x in range(4):
+                if x == p["x"] and y == p["y"]:
+                    print("P ", end="")
+                elif x == box["x"] and y == box["y"]:
+                    print("B ", end="")
+                elif x == 1 and y == 3:
+                    print("G ", end="")
+                else:
+                    print("- ", end="")
+            print()
 
-    move = input("Your move (W/A/S/D)? ").upper()
+        move = input("Your move (W/A/S/D)? ").upper()
 
-    next_px = p["x"]
-    next_py = p["y"]
+        next_px = p["x"]
+        next_py = p["y"]
 
-    dx = 0
-    dy = 0
+        dx = 0
+        dy = 0
 
-    # next_p = p + d
+        # next_p = p + d
 
-    if move == "D":
-        dx = 1
-    elif move == "A":
-        dx = -1
-    elif move == "S":
-        dy = 1
-    elif move == "W":
-        dy = -1
+        if move == "D":
+            dx = 1
+        elif move == "A":
+            dx = -1
+        elif move == "S":
+            dy = 1
+        elif move == "W":
+            dy = -1
 
-    next_px += dx
-    next_py += dy
+        next_px += dx
+        next_py += dy
 
-    if next_px == b["x"] and next_py == b["y"]:
-        b["x"] += dx
-        b["y"] += dy
+        if next_px == box["x"] and next_py == box["y"]:
+            box["x"] += dx
+            box["y"] += dy
 
-    if 0 <= next_px < 4:
-        p["x"] = next_px
+        if 0 <= next_px < 4:
+            p["x"] = next_px
 
-    if 0 <= next_py < 4:
-        p["y"] = next_py
+        if 0 <= next_py < 4:
+            p["y"] = next_py
 
-    if b["x"] == 1 and b["y"] == 3:
-        print("Pass")
-        break
+        if box["x"] == 1 and box["y"] == 3:
+            print("Pass")
+            break
---
+++ Session05/Homework/exe_3.py
@@ -0,0 +1,17 @@
+prices={
+"banana": 4,
+"apple": 2,
+"orange": 1.5,
+"pear": 3
+}
+
+stock={
+"banana": 6,
+"apple": 0,
+"orange": 32,
+"pear": 15
+}
+fruits = list(prices.keys())
+for i in fruits:
+    if fruit==price.get(i,0):
+        print("price: ",price.get(i,0))
